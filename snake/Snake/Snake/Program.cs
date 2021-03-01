cc8e04b193f885125498f3d77fa9caae75dcdd0b
elinaaleksejevski <76990823+elinaaleksejevski@users.noreply.github.com>
2021-03-01 09:57:22 +0200

    Add files via upload

---
+++ snake/Snake/Snake/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Snake
+{
+	class Program
+	{
+		static void Main( string[] args )
+		{
+			Console.SetWindowSize( 80, 25 );
+
+			Walls walls = new Walls( 80, 25 );
+			walls.Draw();
+
+			// Отрисовка точек			
+			Point p = new Point( 4, 5, '*' );
+			Snake snake = new Snake( p, 4, Direction.RIGHT );
+			snake.Draw();
+
+			FoodCreator foodCreator = new FoodCreator( 80, 25, '$' );
+			Point food = foodCreator.CreateFood();
+			FoodCreator foodCreator2 = new FoodCreator(80, 25, 'R');
+			Point food2 = foodCreator2.CreateFood();
+			food.Draw("White");
+
+			Param settings = new Param();
+
+			Sound sound = new Sound(settings.GetResourceFolder());
+			sound.Play();
+			Sound sound1 = new Sound(settings.GetResourceFolder());														  
+			Scores score = new Scores(settings.GetResourceFolder());
+
+			Timer time = new Timer();
+			time.Counter();
+
+
+
+			while (true)
+			{
+				if ( walls.IsHit(snake) || snake.IsHitTail() )
+				{
+					break;
+				}
+				if(snake.Eat( food ) )
+				{
+					food = foodCreator.CreateFood();
+					food2 = foodCreator2.CreateFood();
+					sound1.PlayEat();
+					int check = score.currentFood();
+					int n;
+					if (check > 0 && ((check + 10) % 100) == 0)
+					{
+						food2.Draw("White");
+						n = 1;
+
+					}
+					else if (check > 0 && check % 100 == 0)
+					{
+						food.Draw("White");
+						n = 2;
+					}
+					else
+					{
+						food.Draw("White");
+						n = 1;
+					}
+					score.UpCurrentPoints(n);
+					score.ShowCurrentPoints();
+				}
+				else
+				{
+					snake.Move();
+				}
+
+				Thread.Sleep( 100 );
+				if ( Console.KeyAvailable )
+				{
+					ConsoleKeyInfo key = Console.ReadKey();
+					snake.HandleKey( key.Key );
+				}
+			}
+			sound.Play("gameover");
+			time.CounterStatus(false);
+			score.WriteGameOver();
+			score.userName();
+			score.WriteBestResult();
+			Console.ReadLine();
+
+		}
+
+	}
+}
---
+++ snake/Snake/Snake/Snake.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+	class Snake : Figure
+	{
+		Direction direction;
+
+		public Snake( Point tail, int length, Direction _direction )
+		{
+			direction = _direction;
+			pList = new List<Point>();
+			for ( int i = 0; i < length; i++ )
+			{
+				Point p = new Point( tail );
+				p.Move( i, direction );
+				pList.Add( p );
+			}
+		}
+
+		public void Move()
+		{
+			if (direction != Direction.PAUSE)
+			{
+				Timer.status = true;
+				int xOffset = 80;
+				int yOffset = 15;
+				Console.ForegroundColor = ConsoleColor.Gray;
+				Console.SetCursorPosition(xOffset, yOffset++);
+				WriteText("============================", xOffset, yOffset++);
+				WriteText("Нажмите пробел для паузы ", xOffset + 2, yOffset++);
+				WriteText("============================", xOffset, yOffset++);
+				Point tail = pList.First();
+				pList.Remove(tail);
+				Point head = GetNextPoint();
+				pList.Add(head);
+				tail.Clear();
+				head.Draw("Red");
+			}
+			else
+			{
+				Timer.status = false;
+				int xOffset = 80;
+				int yOffset = 15;
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.SetCursorPosition(xOffset, yOffset++);
+				WriteText("=== Для продолжения игры ===", xOffset, yOffset++);
+				WriteText("= = = = П А У З А = = = =", xOffset + 2, yOffset++);
+				WriteText("= нажмите на любую стрелку =", xOffset, yOffset++);
+			}
+		}
+
+		public Point GetNextPoint()
+		{
+			Point head = pList.Last();
+			Point nextPoint = new Point( head );
+			nextPoint.Move( 1, direction );
+			return nextPoint;
+		}
+
+		public bool IsHitTail()
+		{
+			var head = pList.Last();
+			for(int i = 0; i < pList.Count - 2; i++ )
+			{
+				if ( head.IsHit( pList[ i ] ) )
+					return true;
+			}
+			return false;
+		}
+
+		public void HandleKey(ConsoleKey key)
+		{
+			if ( key == ConsoleKey.LeftArrow )
+				direction = Direction.LEFT;
+			else if ( key == ConsoleKey.RightArrow )
+				direction = Direction.RIGHT;
+			else if ( key == ConsoleKey.DownArrow )
+				direction = Direction.DOWN;
+			else if ( key == ConsoleKey.UpArrow )
+				direction = Direction.UP;
+		}
+
+		public bool Eat( Point food )
+		{
+			Point head = GetNextPoint();
+			if ( head.IsHit( food ) )
+			{
+				food.sym = head.sym;
+				pList.Add( food );
+				return true;
+			}
+			else
+				return false;
+		}
+		static void WriteText(String text, int xOffset, int yOffset)
+		{
+			Console.SetCursorPosition(xOffset, yOffset);
+			Console.WriteLine(text);
+		}
+	}
+}
